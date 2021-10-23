b08aba2bb635a07a793b11072bda19dfc48e4553
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-10-23 18:13:20 +0700

    minor update on display wishlist

--- display-wishlist.py
+++ display-wishlist.py
@@ -1,11 +1,11 @@
 import json
-
+currentUser = str(input("Please enter your email: "))
 tests = open("wishlist.json").read()
 datas = json.loads(tests)
 hello = datas["wishlist"]
 print("Here is your wishlist: ")
 for h in hello:
-    if h['email'] == 'random@yahoo.co.uk':
+    if h['email'] == currentUser:
         url = h['url']
         name = h['name']
         print("\nproduct: ", name)
