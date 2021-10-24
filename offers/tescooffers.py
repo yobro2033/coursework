41e251e27cbd4404bfd110fda57cea79f5e12ef3
yobro2033 <80685605+yobro2033@users.noreply.github.com>
2021-10-24 15:25:59 +0700

    tesco offers support

---
+++ offers/tescooffers.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup as soup
+import json
+
+def TescoOffer():
+    my_url = 'https://www.tesco.com/groceries/en-GB/promotions'
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(my_url, headers=headers)
+    responseCode = str(response)
+    if responseCode == "<Response [200]>":
+        try:
+            productListFound = soup(response.text, 'html.parser')
+            containers = productListFound.findAll("div",{"class":"product-tile-wrapper"})
+            tescoOffer = []
+            for container in containers:
+                productLinkContainer = container.find("a",{"class":"product-image-wrapper"})
+                link = productLinkContainer["href"]
+                image = container.find("div",{"class":"product-image__container"}).find('img')["src"]
+                title = container.find("div",{"class":"product-image__container"}).find('img')["alt"]
+                tescoOffer.append({'store': 'Tesco', 'title': title, 'url': link, 'image': image})
+            return tescoOffer
+        except Exception as e:
+            return tescoOffer
+    else:
+        print(responseCode)
