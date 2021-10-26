b30347640b99aface1517df9cab7a817fcf23828
Quoc Viet Phan <80685605+yobro2033@users.noreply.github.com>
2021-10-26 16:05:53 +0700

    Update displaywishlist.py

--- wishlist/displaywishlist.py
+++ wishlist/displaywishlist.py
@@ -2,7 +2,7 @@ import json
 
 def displayWishlist(currentUser):
     currentUser = str(currentUser)
-    tests = open("wishlist.json").read()
+    tests = open("./wishlist.json").read()
     datas = json.loads(tests)
     hello = datas["wishlist"]
     wishlistdata = []
@@ -12,7 +12,8 @@ def displayWishlist(currentUser):
             name = h['name']
             store = h['store']
             image = h['image']
-            wishlistdata.append({'url': url, 'name': name, 'store': store, 'image': image})
+            price = h['price']
+            wishlistdata.append({'url': url, 'name': name, 'store': store, 'image': image, 'price': price})
         else:
             pass
     return wishlistdata
